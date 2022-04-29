64f5d4e139b93fd5d9723aca29834e8a7bfa9350
Nauviax <nauviax@icloud.com>
2022-04-29 16:45:10 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { Client } from 'boardgame.io/client';
+import { TestGame } from './Game';
+import { INVALID_MOVE } from 'boardgame.io/core';
+
+// To launch this app, type npm start into a console
+
+class TestGameClient {
+	constructor(rootElement) {
+		this.client = Client({ game: TestGame });
+		this.client.start();
+		this.rootElement = rootElement;
+		this.createBoard();
+		this.attachListeners();
+		this.client.subscribe(state => this.update(state));
+	}
+
+	createBoard() {
+		// Create a 5x5 board of cells
+		const rows = [];
+		for (let i = 0; i < 5; i++) {
+			const cells = [];
+			for (let j = 0; j < 5; j++) {
+				const id = 5 * i + j;
+				cells.push(`<td class="cell" data-id="${id}"></td>`);
+			}
+			rows.push(`<tr>${cells.join('')}</tr>`);
+		}
+
+		// Add the HTML to our app <div>.
+		this.rootElement.innerHTML = `<table>${rows.join('')}</table><p class="winner"></p>`;
+	}
+
+	attachListeners() {
+		// This event handler will read the cell id from a cell’s `data-id` attribute and make the `clickCell` move.
+		const handleCellClick = event => {
+			const id = parseInt(event.target.dataset.id);
+			this.client.moves.clickCell(id);
+		};
+		// Attach the event listener to each of the board cells.
+		const cells = this.rootElement.querySelectorAll('.cell');
+		cells.forEach(cell => {
+			cell.onclick = handleCellClick;
+		});
+	}
+
+	update(state) {
+		// Get all the board cells.
+		const cells = this.rootElement.querySelectorAll('.cell');
+		// Update cells to display the values in game state.
+		const playerLocation = state.G.playerLocations[state.ctx.playOrderPos];
+		cells.forEach(cell => {
+			const cellId = parseInt(cell.dataset.id);
+			const cellValue = state.G.cells[Math.floor(cellId / 5)][cellId % 5]; // 2D array yay
+			if (cellValue === null) {
+				const deltaX = cellId % 5 - playerLocation % 5;
+				const deltaY = Math.floor(cellId / 5) - Math.floor(playerLocation / 5);
+				if (!state.ctx.gameover && ((deltaX == 0) != (deltaY == 0))) { // Implements xor
+					cell.textContent = "•";
+				}
+				else {
+					cell.textContent = "";
+				}
+			}
+			else {
+				cell.textContent = cellValue;
+			}
+		});
+		// Get the gameover message element.
+		const messageEl = this.rootElement.querySelector('.winner');
+		// Update the element to show a winner if any.
+		if (state.ctx.gameover) {
+			messageEl.textContent = 'Winner: ' + state.ctx.gameover.winner;
+		} else {
+			messageEl.textContent = '';
+		}
+	}
+}
+
+const appElement = document.getElementById('app');
+const app = new TestGameClient(appElement);
---
+++ src/Game.js
@@ -0,0 +1,465 @@
+export const TestGame = {
+	setup: () => ({
+		cells: Array(5).fill(Array(5).fill(null).fill("0", 0, 1), 0, 1).fill(Array(5).fill(null), 1, 2).fill(Array(5).fill(null), 2, 3).fill(Array(5).fill(null), 3, 4).fill(Array(5).fill(null).fill("1", 4, 5), 4, 5),
+		playerLocations: Array(2).fill(0, 0, 1).fill(24, 1, 2),
+	}),
+	turn: {
+		minMoves: 1,
+		maxMoves: 1,
+	},
+	moves: {
+		clickCell: (G, ctx, id) => {
+			if (G.cells[Math.floor(id / 5)][id % 5] !== null) { return INVALID_MOVE; } // Must be empty
+			const player = ctx.currentPlayer;
+			const playerLocation = G.playerLocations[player];
+			const deltaX = id % 5 - playerLocation % 5;
+			const deltaY = Math.floor(id / 5) - Math.floor(playerLocation / 5);
+			if (deltaX != 0 && deltaY != 0) { return INVALID_MOVE; } // Must be in a straight line
+			G.cells[Math.floor(playerLocation / 5)][playerLocation % 5] = null;
+			G.cells[Math.floor(id / 5)][id % 5] = player;
+			G.playerLocations[player] = id;
+
+			const size = 15;
+			var newMap = InitializeMap(size); // Oh God here we go (!!!)
+			for (let ii = 0; ii < size; ii++) {
+				var row = '';
+				for (let jj = 0; jj < size; jj++) {
+					row += newMap[jj][ii].tile.id;
+				}
+				console.log(row);
+				//console.log(" ");
+			}
+
+		},
+	},
+	endIf: (G, ctx) => {
+		let winner = IsVictory(G.playerLocations, ctx.currentPlayer);
+		if (winner !== null) {
+			return { winner };
+		}
+	},
+	ai: { // Very simple for now
+		enumerate: (G, ctx) => {
+			let moves = [];
+			const playerLocation = G.playerLocations[ctx.playOrderPos];
+
+			for (let ii = 0; ii < 25; ii++) {
+				if (G.cells[Math.floor(ii / 5)][ii % 5] === null) {
+					const deltaX = ii % 5 - playerLocation % 5;
+					const deltaY = Math.floor(ii / 5) - Math.floor(playerLocation / 5);
+					if (deltaX == 0 || deltaY == 0) {
+						moves.push({ move: 'clickCell', args: [ii] });
+					}
+				}
+			}
+			return moves;
+		},
+	},
+};
+
+function IsVictory(playerLocations, currentPlayer) {
+	// A player wins if it moves next to another player
+	const playerDistance = Math.abs(playerLocations[0] - playerLocations[1]);
+	if (playerDistance == 1 || playerDistance == 5) {
+		return currentPlayer;
+	}
+	else {
+		return null;
+	}
+};
+
+
+// Generation code below
+
+function InitializeMap(mapSize) { // Creates an NxN board of cells, and gives them a copy of tileList each. Calls GenerateMap when finished
+	// Generate a NxN board of cells, where each cell has a copy of the tileList
+	// Call GenerateMap, passing in the created board
+	// If it returns false, create a new board and call again
+	// If it returns true, return the map
+
+	while (true) { // Loop until map is generated, then return from inside loop
+		// Debug
+		console.log("Generating new map...");
+		let map = []; // The map to be generated (Will overwrite previous map, if it existed)
+		for (let ii = 0; ii < mapSize; ii++) {
+			map[ii] = [];
+			for (let jj = 0; jj < mapSize; jj++) {
+				map[ii][jj] = new CellData(ii, jj);
+			}
+		}
+		let output = GenerateMap(map, mapSize); // Output is a bool followed by the map. If map is invalid, output is {false,null}
+		console.log("Generated map: " + output.success);
+		if (output.success) { // Return the map if it is valid
+			return output.map;
+		}
+	}
+}
+
+function GenerateMap(map, mapSize) { // Checks if every cell has selected a tile. If a tile exists that has not yet selected a tile, pick one at random and choose an available tile for it at random, then update the tiles around it
+	// Check if every cell has selected a tile
+	// If a tile exists with no tile,
+	// Pick a random tile from it's list
+	// Update the tiles around it (Check if a tile exists in each direction first, edge of map and all that)
+	// If an update returns false, return false, null so InitializeMap can retry
+	// Loop until all tiles have a selected tile, then return true, <The finished map>
+
+	while (true) { // Loop until a valid map is generated. Will return from inside this loop
+		let nullTiles = []; // A list of tiles that have no tile selected
+		for (let ii = 0; ii < map.length; ii++) {
+			for (let jj = 0; jj < map[ii].length; jj++) {
+				if (map[ii][jj].tile == null) {
+					nullTiles.push(map[ii][jj]);
+				}
+			}
+		}
+		if (nullTiles.length == 0) { return { success: true, map }; } // If there are no null tiles, we're done
+		else { // If there are still null tiles, pick one at random and select a tile for it
+
+			let tile = nullTiles[Math.floor(Math.random() * nullTiles.length)]; // Pick a random tile that has no tile selected
+
+			// Temp code for weighting
+			/*
+			outside = 300;
+			inside = 150;
+			wallStraight0a = 70;
+			wallStraight1a = 70;
+			wallStraight0b = 70;
+			wallStraight1b = 70;
+			wallCorner0a = 30;
+			wallCorner1a = 30;
+			wallCorner2a = 30;
+			wallCorner3a = 30;
+			wallCorner0b = 10;
+			wallCorner1b = 10;
+			wallCorner2b = 10;
+			wallCorner3b = 10;
+			wallDoor0a = 5;
+			wallDoor1a = 5;
+			wallDoor0b = 5;
+			wallDoor1b = 5;
+			
+
+			*/
+
+			// If tileList contains inside or outside, prefer picking those
+			if (tile.tileList.includes(outside)) {
+				let chance = 0.7; // Chance to just pick outside
+				if (Math.random() < chance) {
+					tile.tile = outside;
+				}
+				else { // Otherwise, pick normally
+					let tileIndex = Math.floor(Math.random() * tile.tileList.length); // Pick a random tile from the list
+					tile.tile = tile.tileList[tileIndex]; // Select the tile
+				}
+			}
+			else { // Otherwise, pick normally
+				let tileIndex = Math.floor(Math.random() * tile.tileList.length); // Pick a random tile from the list
+				tile.tile = tile.tileList[tileIndex]; // Select the tile
+			}
+			tile.tileList = []; // Remove all tiles from the tilelist (so it can't be selected again)
+
+			// Debug
+			console.log("SET tile at " + tile.x + "," + tile.y + ": " + tile.tile.id);
+
+			// Update the tiles around it
+			let success = true; // If false, the map is unsolvable (If map is unsolvalbe, InitializeMap will retry)
+			if (tile.y != 0) { success = UpdateTile(map[tile.x][tile.y - 1], map, mapSize, 0); } // Update the tile above
+			if (!success) { return { success: false, map: null }; }
+			if (tile.y != mapSize - 1) { success = UpdateTile(map[tile.x][tile.y + 1], map, mapSize, 0); } // Update the tile below
+			if (!success) { return { success: false, map: null }; }
+			if (tile.x != 0) { success = UpdateTile(map[tile.x - 1][tile.y], map, mapSize, 0); } // Update the tile to the left
+			if (!success) { return { success: false, map: null }; }
+			if (tile.x != mapSize - 1) { success = UpdateTile(map[tile.x + 1][tile.y], map, mapSize, 0); } // Update the tile to the right
+			if (!success) { return { success: false, map: null }; }
+		}
+	}
+}
+function UpdateTile(tile, map, mapSize, depth) {
+	// If this tile has only one option, select it
+	// If this tile has multiple options, check each direction and remove my tiles that don't fit
+	// A tile option doesn't fit if the tile in that direction either has a tile which is incompatible, or the tile in that direction contains only options that are incompatible
+	// A tile is incompatible with another tile if the sides do not match
+	// If any change was made to this tile, either selecting a tile or removing options, update all tiles around it
+
+	var newDepth = depth++; // Limit is 10
+	if (depth > 10) { console.log("Depth Reached"); return true; } // If we've reached the limit, don't check this tile, but also don't throw an error
+
+	var stateChanged = false; // True if tiles around this tile should be updated as a result of a change
+	if (tile.tileList.length == 1) { // If this tile has only one option, select it
+		tile.tile = tile.tileList[0];
+		tile.tileList = [];
+		stateChanged = true;
+
+		// Debug
+		console.log("Selected tile at " + tile.x + "," + tile.y + ": " + tile.tile.id);
+	}
+	else { // If this tile has multiple options, check each direction and remove my tiles that don't fit
+
+		// This gets ugly, but it's basically the same code 4 times in a row, with different directions
+
+		for (let ii = 0; ii < tile.tileList.length; ii++) { // For each possible tile option
+			let myTileOption = tile.tileList[ii]; // Currently checking
+			let remove = false; // True if this tile option should be removed
+
+			if (tile.y != 0) { // Checking above tile
+				let aboveTile = map[tile.x][tile.y - 1];
+				if (aboveTile.tile != null) { // If the tile above has a tile, check if it's side matches this side
+					if (aboveTile.tile.sides[2] != myTileOption.sides[0]) { // If the sides don't match, remove this tile option
+						remove = true;
+					}
+					if (wallsLR.includes(aboveTile.tile) && wallsLR.includes(myTileOption)) { // If both tiles are LR walls, remove this tile option
+						remove = true;
+					} else {
+						if (corners.includes(aboveTile.tile) && corners.includes(myTileOption)) { // If both tiles are corners, remove this tile option
+							remove = true;
+						}
+					}
+				}
+				else { // Otherwise, loop through this tiles options and see if at least one tile is compatible. If not, remove this tile option
+					remove = true; // Default to removing this tile, unless at least one tile is compatible
+					for (let jj = 0; jj < aboveTile.tileList.length; jj++) {
+						if (aboveTile.tileList[jj].sides[2] == myTileOption.sides[0]) { // If the tile is compatible, stop checking
+							if (!(wallsLR.includes(aboveTile.tileList[jj]) && wallsLR.includes(myTileOption))) { // Must also pass the LR wall check
+								if (!(corners.includes(aboveTile.tileList[jj]) && corners.includes(myTileOption))) { // Must also pass the corner check
+									remove = false; // The tile is saved, yay
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (remove) { // Remove this tile option if it should be removed, then continue to the next for loop item
+				tile.tileList.splice(ii, 1);
+				ii--;
+				stateChanged = true;
+				continue;
+			}
+
+			if (tile.y != mapSize - 1) { // Checking below tile
+				let belowTile = map[tile.x][tile.y + 1];
+				if (belowTile.tile != null) { // If the tile below has a tile, check if it's side matches this side
+					if (belowTile.tile.sides[0] != myTileOption.sides[2]) { // If the sides don't match, remove this tile option
+						remove = true;
+					}
+					if (wallsLR.includes(belowTile.tile) && wallsLR.includes(myTileOption)) { // If both tiles are LR walls, remove this tile option
+						remove = true;
+					} else {
+						if (corners.includes(belowTile.tile) && corners.includes(myTileOption)) { // If both tiles are corners, remove this tile option
+							remove = true;
+						}
+					}
+				}
+				else { // Otherwise, loop through this tiles options and see if at least one tile is compatible. If not, remove this tile option
+					remove = true; // Default to removing this tile, unless at least one tile is compatible
+					for (let jj = 0; jj < belowTile.tileList.length; jj++) {
+						if (belowTile.tileList[jj].sides[0] == myTileOption.sides[2]) { // If the tile is compatible, stop checking
+							if (!(wallsLR.includes(belowTile.tileList[jj]) && wallsLR.includes(myTileOption))) { // Must also pass the LR wall check
+								if (!(corners.includes(belowTile.tileList[jj]) && corners.includes(myTileOption))) { // Must also pass the corner check
+									remove = false; // The tile is saved, yay
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (remove) { // Remove this tile option if it should be removed, then continue to the next for loop item
+				tile.tileList.splice(ii, 1);
+				ii--;
+				stateChanged = true;
+				continue;
+			}
+
+			if (tile.x != 0) { // Checking left tile
+				let leftTile = map[tile.x - 1][tile.y];
+				if (leftTile.tile != null) { // If the tile left has a tile, check if it's side matches this side
+					if (leftTile.tile.sides[1] != myTileOption.sides[3]) { // If the sides don't match, remove this tile option
+						remove = true;
+					}
+					if (wallsUD.includes(leftTile.tile) && wallsUD.includes(myTileOption)) { // If both tiles are UD walls, remove this tile option
+						remove = true;
+					} else {
+						if (corners.includes(leftTile.tile) && corners.includes(myTileOption)) { // If both tiles are corners, remove this tile option
+							remove = true;
+						}
+					}
+				}
+				else { // Otherwise, loop through this tiles options and see if at least one tile is compatible. If not, remove this tile option
+					remove = true; // Default to removing this tile, unless at least one tile is compatible
+					for (let jj = 0; jj < leftTile.tileList.length; jj++) {
+						if (leftTile.tileList[jj].sides[1] == myTileOption.sides[3]) { // If the tile is compatible, stop checking
+							if (!(wallsUD.includes(leftTile.tile) && wallsUD.includes(myTileOption))) { // Must also pass the UD wall check
+								if (!(corners.includes(leftTile.tile) && corners.includes(myTileOption))) { // Must also pass the corner check
+									remove = false; // The tile is saved, yay
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (remove) { // Remove this tile option if it should be removed, then continue to the next for loop item
+				tile.tileList.splice(ii, 1);
+				ii--;
+				stateChanged = true;
+				continue;
+			}
+
+			if (tile.x != mapSize - 1) { // Checking right tile
+				let rightTile = map[tile.x + 1][tile.y];
+				if (rightTile.tile != null) { // If the tile right has a tile, check if it's side matches this side
+					if (rightTile.tile.sides[3] != myTileOption.sides[1]) { // If the sides don't match, remove this tile option
+						remove = true;
+					}
+					if (wallsUD.includes(rightTile.tile) && wallsUD.includes(myTileOption)) { // If both tiles are UD walls, remove this tile option
+						remove = true;
+					} else {
+						if (corners.includes(rightTile.tile) && corners.includes(myTileOption)) { // If both tiles are corners, remove this tile option
+							remove = true;
+						}
+					}
+				}
+				else { // Otherwise, loop through this tiles options and see if at least one tile is compatible. If not, remove this tile option
+					remove = true; // Default to removing this tile, unless at least one tile is compatible
+					for (let jj = 0; jj < rightTile.tileList.length; jj++) {
+						if (rightTile.tileList[jj].sides[3] == myTileOption.sides[1]) { // If the tile is compatible, stop checking
+							if (!(wallsUD.includes(rightTile.tile) && wallsUD.includes(myTileOption))) { // Must also pass the UD wall check
+								if (!(corners.includes(rightTile.tile) && corners.includes(myTileOption))) { // Must also pass the corner check
+									remove = false; // The tile is saved, yay
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (remove) { // Remove this tile option if it should be removed, then continue to the next for loop item
+				tile.tileList.splice(ii, 1);
+				ii--;
+				stateChanged = true;
+				continue;
+			}
+		}
+	}
+	if (tile.tileList.length == 1) { // Check if the tile now has only one tile option, and select it if it does
+		tile.tile = tile.tileList[0];
+		tile.tileList = [];
+		stateChanged = true; // This should already be true, but no harm in setting it again
+
+		// Debug
+		console.log("Selected tile at " + tile.x + "," + tile.y + ": " + tile.tile.id);
+	}
+	if (tile.tileList.length == 0 && tile.tile == null) { // If there are no tile options left, and the tile is not set, then the map is unsolvable
+		return false; // Start again
+	}
+	if (stateChanged) { // If the state changed, check the surrounding tiles
+		let success = true; // If false, the map is unsolvable
+		if (tile.y != 0) { success = UpdateTile(map[tile.x][tile.y - 1], map, mapSize, newDepth); } // Update the tile above
+		if (!success) { return false; }
+		if (tile.y != mapSize - 1) { success = UpdateTile(map[tile.x][tile.y + 1], map, mapSize, newDepth); } // Update the tile below
+		if (!success) { return false; }
+		if (tile.x != 0) { success = UpdateTile(map[tile.x - 1][tile.y], map, mapSize, newDepth); } // Update the tile to the left
+		if (!success) { return false; }
+		if (tile.x != mapSize - 1) { success = UpdateTile(map[tile.x + 1][tile.y], map, mapSize, newDepth); } // Update the tile to the right
+		if (!success) { return false; }
+	}
+	return true; // Successful update
+}
+
+
+// Map tiles below
+
+function CellData(x, y) { // Stores the possible tiles for a cell, the location of this cell, and a reference to the cells around itself (To be filled in later, not in constructor)
+	this.tile = null;
+	this.tileList = [outside, inside, wallStraight0a, wallStraight1a, wallStraight0b, wallStraight1b, wallCorner0a, wallCorner1a, wallCorner2a, wallCorner3a, wallCorner0b, wallCorner1b, wallCorner2b, wallCorner3b, wallDoor0a, wallDoor1a, wallDoor0b, wallDoor1b];
+	this.x = x;
+	this.y = y;
+}
+
+// A list of all the below objects, to be copied to each cell during generation
+
+
+const outside = { // Generic tile
+	id: ' ',
+	sides: Array(4).fill("0"),
+};
+const inside = { // Generic tile, will be assigned a building id after generation
+	id: '█',
+	sides: Array(4).fill("1"),
+};
+
+
+const wallStraight0a = { // Top to bottom, others are rotated clockwise. 'a' means inside is above or to the right, 'b' means inside is below or to the left
+	id: '│',
+	sides: Array(4).fill("2a", 0, 1).fill("1", 1, 2).fill("2a", 2, 3).fill("0", 3, 4),
+};
+const wallStraight1a = {
+	id: '─',
+	sides: Array(4).fill("1", 0, 1).fill("2a", 1, 2).fill("0", 2, 3).fill("2a", 3, 4),
+};
+const wallStraight0b = {
+	id: '│',
+	sides: Array(4).fill("2b", 0, 1).fill("0", 1, 2).fill("2b", 2, 3).fill("1", 3, 4),
+};
+const wallStraight1b = {
+	id: '─',
+	sides: Array(4).fill("0", 0, 1).fill("2b", 1, 2).fill("1", 2, 3).fill("2b", 3, 4),
+};
+
+
+const wallCorner0a = { // Top to right, all others are rotated clockwise. 'a' means corner is inside, 'b' means  corner is outside
+	id: '└',
+	sides: Array(4).fill("2a", 0, 1).fill("2a", 1, 2).fill("0", 2, 3).fill("0", 3, 4),
+};
+const wallCorner1a = {
+	id: '┌',
+	sides: Array(4).fill("0", 0, 1).fill("2a", 1, 2).fill("2a", 2, 3).fill("0", 3, 4),
+};
+const wallCorner2a = {
+	id: '┐',
+	sides: Array(4).fill("0", 0, 1).fill("0", 1, 2).fill("2a", 2, 3).fill("2a", 3, 4),
+};
+const wallCorner3a = {
+	id: '┘',
+	sides: Array(4).fill("2a", 0, 1).fill("0", 1, 2).fill("0", 2, 3).fill("2a", 3, 4),
+};
+const wallCorner0b = {
+	id: '└',
+	sides: Array(4).fill("2b", 0, 1).fill("2b", 1, 2).fill("1", 2, 3).fill("1", 3, 4),
+};
+const wallCorner1b = {
+	id: '┌',
+	sides: Array(4).fill("1", 0, 1).fill("2b", 1, 2).fill("2b", 2, 3).fill("1", 3, 4),
+};
+const wallCorner2b = {
+	id: '┐',
+	sides: Array(4).fill("1", 0, 1).fill("1", 1, 2).fill("2b", 2, 3).fill("2b", 3, 4),
+};
+const wallCorner3b = {
+	id: '┘',
+	sides: Array(4).fill("2b", 0, 1).fill("1", 1, 2).fill("1", 2, 3).fill("2b", 3, 4),
+};
+
+
+const wallDoor0a = { // Follows same rules as wallStraight but, yknow, it's also a door
+	id: '╠',
+	sides: Array(4).fill("2a", 0, 1).fill("1", 1, 2).fill("2a", 2, 3).fill("0", 3, 4),
+};
+const wallDoor1a = {
+	id: '╩',
+	sides: Array(4).fill("1", 0, 1).fill("2a", 1, 2).fill("0", 2, 3).fill("2a", 3, 4),
+};
+const wallDoor0b = {
+	id: '╣',
+	sides: Array(4).fill("2b", 0, 1).fill("0", 1, 2).fill("2b", 2, 3).fill("1", 3, 4),
+};
+const wallDoor1b = {
+	id: '╦',
+	sides: Array(4).fill("0", 0, 1).fill("2b", 1, 2).fill("1", 2, 3).fill("2b", 3, 4),
+};
+
+// Some constant lists for checking if a tile is in a certian category
+const wallsLR = [wallStraight1a, wallStraight1b, wallDoor1a, wallDoor1b]; // Walls can not be parralel and next to each other
+const wallsUD = [wallStraight0a, wallStraight0b, wallDoor0a, wallDoor0b];
+const corners = [wallCorner0a, wallCorner1a, wallCorner2a, wallCorner3a, wallCorner0b, wallCorner1b, wallCorner2b, wallCorner3b]; // Corners can not be next to each other
