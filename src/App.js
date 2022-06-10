5b75a89b6fe96045f38b14a3c9033c213776b3f7
Nauviax <nauviax@icloud.com>
2022-06-10 17:13:23 +1200

    Working on making checkbox list pretty. Also added margins to page

--- src/App.js
+++ src/App.js
@@ -131,19 +131,19 @@ class TestGameClient {
 		this.rootElement.innerHTML += `<p class="winner"></p>`;
 
 
-		let playerStringTemp = "";
-		for (let ii = 0; ii < state.ctx.numPlayers; ii++) { // For each player on the board,
-			playerStringTemp += "Player " + ii + " ";
-		}
-		this.rootElement.innerHTML += `<p>${playerStringTemp}</p>`;
+		// let playerStringTemp = "";
+		// for (let ii = 0; ii < state.ctx.numPlayers; ii++) { // For each player on the board,
+		// 	playerStringTemp += "Player " + ii + " ";
+		// }
+		// this.rootElement.innerHTML += `<p>${playerStringTemp}</p>`;
 
 		// Create and draw checkboxs for everything
 		for (let hh = 0; hh < state.G._cardsInPlay.length; hh++) {
-			for (let ii = 0; ii < state.G._cardsInPlay[hh].length; ii++) { // For each room in the room in game,
-				this.rootElement.innerHTML += state.G._cardsInPlay[hh][ii] + ': '; // Write the name of each room next to the checkbox
+			for (let ii = 0; ii < state.G._cardsInPlay[hh].length; ii++) { // For each card in the game,
+				this.rootElement.innerHTML += `<p>${state.G._cardsInPlay[hh][ii]}: </p>`; // Write the name of each card next to the checkbox
 				// Player for loop
 				for (let jj = 0; jj < state.ctx.numPlayers; jj++) { // For each player on the board,
-					this.rootElement.innerHTML += `<input type="checkbox" id=${jj}"></input>`; // Draw a checkbox for each room in game
+					this.rootElement.innerHTML += `<input type="checkbox" id=${jj}>P${jj}</input>`; // Draw a checkbox for each room in game
 				}
 				this.rootElement.innerHTML += ' <br/>'; // <br> to add a new line 
 			}
